366261f8145418d295894d905bc0dcb29f90cdc8
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2021-06-02 15:41:04 +0100

    control environments more crudely

--- allRProcessing.R
+++ allRProcessing.R
@@ -9,36 +9,40 @@ open_sinks <- function(filestart){
   sink(msg_file, append = TRUE, type = "message") # Writing console output to log file
 }
 
-env <- environment() # need to record environment to be able to clear it - this changes when sourcing the file.
-
 reset_for_next <- function(){
   sink()
   sink(type = "message")
-  rm(list = ls(env)[!(ls(env) %in% c("name_of_current_run", "reset_for_next", "open_sinks", "env"))], envir = env)
 }
+
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 # PREPROCESSING========================================================
 open_sinks("ukbDataPrep/plots/")
 source("ukbDataPrep/scripts/preprocessing.R")
 reset_for_next()
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 # MAIN ANALYSIS========================================================
 open_sinks("epiAnalysis/plots/")
 source("epiAnalysis/scripts/analysis.R")
 reset_for_next()
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 # OTHER MISCELLANEOUS PARTS TO ANALYSIS================================
 open_sinks("classifierDevelopment/plots/")
 source("classifierDevelopment/scripts/reportingPerformance/check_precision_and_recall.R")
 reset_for_next()
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 open_sinks("ukbAccProcessing/plots/")
 source("ukbAccProcessing/scripts/acc_plots_over_the_day.R")
 reset_for_next()
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 open_sinks("epiAnalysis/plots/ggtern_")
 source("epiAnalysis/scripts/ggtern_plots.R")
 reset_for_next()
+rm(list = ls()[!(ls() %in% c("name_of_current_run", "reset_for_next", "open_sinks"))])
 
 # OUTPUT DETAILS OF ENVIRONMENT===================================================================
 renv::snapshot(
