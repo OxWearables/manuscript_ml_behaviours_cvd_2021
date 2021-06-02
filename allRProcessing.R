05de2ea8e9477b45079f24537321c3f5649a8c76
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2021-06-02 10:51:35 +0100

    reorganise sinks of messages

--- allRProcessing.R
+++ allRProcessing.R
@@ -1,7 +1,14 @@
 # SET NAME OF CURRENT RUN==============================================
 name_of_current_run <- paste0(Sys.Date(), "_paper_ready_run_")
 
-# SET UP FUNCTION FOR CLEARING AFTER EACH================================
+# SET UP FUNCTIONS FOR SINKING CONSOLE OUTPUT AND CLEARING AFTER EACH================================
+open_sinks <- function(filestart){
+  output_file <- file(paste0(filestart, name_of_current_run, "_console_output.txt"), open = "a")
+  msg_file <- file(paste0(filestart, name_of_current_run, "_message_output.txt"), open = "a")
+  sink(output_file, append = TRUE, type = "output") # Writing console output to log file
+  sink(msg_file, append = TRUE, type = "message") # Writing console output to log file
+}
+
 reset_for_next <- function(){
   sink()
   sink(type = "message")
@@ -10,30 +17,25 @@ reset_for_next <- function(){
 reset_for_next()
 
 # PREPROCESSING========================================================
-sink(paste0("ukbDataPrep/plots/", name_of_current_run, "_console_ouput.txt"), append = TRUE, type = "output") # Writing console output to log file
-sink(paste0("ukbDataPrep/plots/", name_of_current_run, "_console_message_ouput.txt"), append = TRUE, type = "message") # Writing console output to log file
+open_sinks("ukbDataPrep/plots/")
 source("ukbDataPrep/scripts/preprocessing.R")
 reset_for_next()
 
 # MAIN ANALYSIS========================================================
-sink(paste0("epiAnalysis/plots/", name_of_current_run, "_console_ouput.txt"), append = TRUE, type = "output") # Writing console output to log file
-sink(paste0("epiAnalysis/plots/", name_of_current_run, "_console_message_ouput.txt"), append = TRUE, type = "message") # Writing console output to log file
+open_sinks("epiAnalysis/plots/")
 source("epiAnalysis/scripts/analysis.R")
 reset_for_next()
 
 # OTHER MISCELLANEOUS PARTS TO ANALYSIS================================
-sink(paste0("classifierDevelopment/plots/", name_of_current_run, "_console_ouput.txt"), append = TRUE, type = "output") # Writing console output to log file
-sink(paste0("classifierDevelopment/plots/", name_of_current_run, "_console_message_ouput.txt"), append = TRUE, type = "message") # Writing console output to log file
+open_sinks("classifierDevelopment/plots/")
 source("classifierDevelopment/scripts/reportingPerformance/check_precision_and_recall.R")
 reset_for_next()
 
-sink(paste0("ukbAccProcessing/plots/", name_of_current_run, "_console_ouput.txt"), append = TRUE, type = "output") # Writing console output to log file
-sink(paste0("ukbAccProcessing/plots/", name_of_current_run, "_console_message_ouput.txt"), append = TRUE, type = "message") # Writing console output to log file
+open_sinks("ukbAccProcessing/plots/")
 source("ukbAccProcessing/scripts/acc_plots_over_the_day.R")
 reset_for_next()
 
-sink(paste0("epiAnalysis/plots/", name_of_current_run, "_ggtern_console_ouput.txt"), append = TRUE, type = "output") # Writing console output to log file
-sink(paste0("epiAnalysis/plots/", name_of_current_run, "_ggtern_console_message_ouput.txt"), append = TRUE, type = "message") # Writing console output to log file
+open_sinks("epiAnalysis/plots/ggtern_")
 source("epiAnalysis/scripts/ggtern_plots.R")
 reset_for_next()
 
