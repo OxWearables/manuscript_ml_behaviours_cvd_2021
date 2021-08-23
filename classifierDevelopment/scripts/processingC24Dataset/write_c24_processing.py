d0c9a34377aef5b0c08ba953b826c654a3b1d6f9
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2021-08-23 16:24:26 +0100

    remove references to processing C24 dataset

--- classifierDevelopment/scripts/processingC24Dataset/write_c24_processing.py
+++
@@ -1,2 +0,0 @@
-from accelerometer import accUtils
-accUtils.writeStudyAccProcessCmds(accDir = "/well/doherty/users/llz512/home/paperRW2021/classifierDevelopment/inputData/c24Data/data/", outDir = "/well/doherty/users/llz512/home/paperRW2021/classifierDevelopment/inputData/c24Processed/data/", cmdsFile = "c24_processing_cmds_270521.txt", accExt = ".csv", cmdOptions = "--deleteIntermediateFiles False --activityClassification False --csvTimeFormat 'yyyy-MM-dd HH:mm:ss.SSSSSS'") 
